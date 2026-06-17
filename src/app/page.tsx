7c660162e89039561a88da7ee3a4f95b9f933a6f
ctrujillo12 <cheyenneettrujillo@gmail.com>
2026-06-17 00:12:19 -0700

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,9 +188,9 @@ export default async function Home() {
             href="https://tualmi.com/invite"
             style={{ display: 'inline-block', position: 'relative', textDecoration: 'none', cursor: 'pointer' }}
           >
-            <svg viewBox="0 0 240 52" width="210" height="45" style={{ display: 'block' }}>
+            <svg viewBox="0 0 250 68" width="222" height="60" style={{ display: 'block' }}>
               <path
-                d="M 230,26 L 220,30 L 215,37 L 193,37 L 175,45 L 147,41 L 120,48 L 93,41 L 65,45 L 47,37 L 25,37 L 20,30 L 10,26 L 20,22 L 25,15 L 47,15 L 65,7 L 93,11 L 120,4 L 147,11 L 175,7 L 193,15 L 215,15 L 220,22 Z"
+                d="M 125,6 L 144.1,14.4 L 170.2,8.1 L 179.4,17.4 L 208.4,14.2 L 206.5,22.9 L 234,23.3 L 221.1,30.1 L 243,34 L 221.1,37.9 L 234,44.7 L 206.5,45.1 L 208.4,53.8 L 179.4,50.6 L 170.2,59.9 L 144.1,53.6 L 125,62 L 105.9,53.6 L 79.8,59.9 L 70.6,50.6 L 41.6,53.8 L 43.5,45.1 L 16,44.7 L 28.9,37.9 L 7,34 L 28.9,30.1 L 16,23.3 L 43.5,22.9 L 41.6,14.2 L 70.6,17.4 L 79.8,8.1 L 105.9,14.4 Z"
                 fill="transparent"
                 stroke="#C94468"
                 strokeWidth="1.5"
@@ -203,9 +203,9 @@ export default async function Home() {
               transform: 'translate(-50%, -50%)',
               color: '#C94468',
               fontFamily: sans,
-              fontSize: '8.5px',
+              fontSize: '8px',
               fontWeight: 500,
-              letterSpacing: '0.15em',
+              letterSpacing: '0.14em',
               textTransform: 'uppercase',
               whiteSpace: 'nowrap',
             }}>
