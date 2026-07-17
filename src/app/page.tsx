91360903837f4a3d6035966c1778c4fd6a753a8b
ctrujillo12 <cheyenneettrujillo@gmail.com>
2026-07-17 16:04:19 -0700

    mo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -396,7 +396,7 @@ export default function Home() {
                 textTransform: 'lowercase',
               }}
             >
-              @tualmi.outdoors
+              @tualmioutdoors
             </h2>
           </a>
 
